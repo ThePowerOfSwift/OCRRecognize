0f9345d4914acae012260978973540143102b80b
Tolecen <taox.l@163.com>
2017-06-18 15:07:11 +0800

    first commit

---
+++ AipOcrSdk/AipOcrSdk/ViewController/ResultViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResultViewController.h
+//  AipOcrSdk
+//
+//  Created by Tolecen on 2017/6/17.
+//  Copyright © 2017年 baidu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResultViewController : UIViewController
+@property (nonatomic,strong)NSString * resultStr;
+@end
